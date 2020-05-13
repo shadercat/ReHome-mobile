3b81be5df66b7333a4853cc7eee70aea948c76d7
shadercat <shadercatin@gmail.com>
2020-05-13 21:39:23 +0300

    - add device class; - add device recycler list adapter; - add device item layout; - update strings res;

---
+++ app/src/main/java/com/example/rehome/models/Device.java
@@ -0,0 +1,52 @@
+package com.example.rehome.models;
+
+public class Device {
+    private String status;
+    private String state;
+    private String name;
+    private String code;
+
+    public Device() {
+
+    }
+
+    public Device(String name, String code, String status, String state) {
+        this.name = name;
+        this.code = code;
+        this.state = state;
+        this.status = status;
+    }
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
---
+++ app/src/main/java/com/example/rehome/ui/devices/DeviceListAdapter.java
@@ -0,0 +1,87 @@
+package com.example.rehome.ui.devices;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.rehome.R;
+import com.example.rehome.models.Device;
+
+import java.util.List;
+
+
+public class DeviceListAdapter extends RecyclerView.Adapter<DeviceListAdapter.DeviceViewHolder> {
+    private LayoutInflater inflater;
+    private List<Device> devices;
+    private ItemActionHandler onClickHandler;
+    private Context context;
+
+    DeviceListAdapter(Context context, List<Device> devices) {
+        this.devices = devices;
+        this.inflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public DeviceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.device_item, parent, false);
+        return new DeviceViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DeviceViewHolder holder, int position) {
+        if (position != devices.size()) {
+            Device device = devices.get(holder.getAdapterPosition());
+            holder.name.setText(device.getName());
+            holder.code.setText(context.getString(R.string.device_code_p, device.getCode()));
+            holder.state.setText(device.getState());
+            holder.status.setText(device.getStatus());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return devices == null ? 0 : devices.size();
+    }
+
+    public void setOnClickHandler(ItemActionHandler handler) {
+        this.onClickHandler = handler;
+    }
+
+    public void setDevicesList(List<Device> devices) {
+        this.devices = devices;
+        notifyDataSetChanged();
+    }
+
+    interface ItemActionHandler {
+        void onItemClick(int position);
+    }
+
+    public class DeviceViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        final TextView name, code, state, status;
+
+        public DeviceViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            name = itemView.findViewById(R.id.device_name);
+            code = itemView.findViewById(R.id.device_code);
+            state = itemView.findViewById(R.id.device_state);
+            status = itemView.findViewById(R.id.device_status);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (onClickHandler != null) {
+                onClickHandler.onItemClick(getLayoutPosition());
+            }
+        }
+    }
+}
