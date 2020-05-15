786b5874c750f08e2789311fd9b369e6ae74e9ce
shadercat <shadercatin@gmail.com>
2020-05-15 22:03:10 +0300

    - implemented UserViewModel;

--- app/src/main/java/com/example/rehome/ui/user/UserViewModel.java
+++ app/src/main/java/com/example/rehome/ui/user/UserViewModel.java
@@ -1,7 +1,43 @@
 package com.example.rehome.ui.user;
 
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.rehome.ConstantUrls;
+import com.example.rehome.connection.HttpClient;
+import com.example.rehome.connection.JsonConverter;
+import com.example.rehome.connection.ResponseHandler;
+import com.example.rehome.models.User;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
 public class UserViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    private MutableLiveData<User> user;
+
+    public LiveData<User> getData() {
+        if (user == null) {
+            user = new MutableLiveData<>();
+            loadData();
+        }
+        return user;
+    }
+
+    private void loadData() {
+        HttpClient.get(ConstantUrls.userData, null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                if (ResponseHandler.IsSuccessed(response)) {
+                    Log.d("DEV_POINT", "requested user data success");
+                    user.postValue(JsonConverter.UserData(response));
+                }
+            }
+        });
+    }
 }
