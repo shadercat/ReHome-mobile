a0506daa19cf53d0406415d198b3573f13600844
shadercat <shadercatin@gmail.com>
2020-05-15 17:43:59 +0300

    - add ResourceGroup class; - add JsonConverter method for ResourceGroup;

--- app/src/main/java/com/example/rehome/connection/JsonConverter.java
+++ app/src/main/java/com/example/rehome/connection/JsonConverter.java
@@ -3,6 +3,7 @@ package com.example.rehome.connection;
 import android.util.Log;
 
 import com.example.rehome.models.Device;
+import com.example.rehome.models.ResourceGroup;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,6 +13,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JsonConverter {
+    private static final String DEV_ERR = "DEV_ERR_JsonConverter";
+
     public static List<Device> DeviceList(JSONObject object) {
         List<Device> devices = new ArrayList<>();
         try {
@@ -28,8 +31,26 @@ public class JsonConverter {
                 devices.add(new Device(name, code, status, state));
             }
         } catch (JSONException e) {
-            Log.e("DEV_ERR", "JSON parse error: ", e);
+            Log.e(DEV_ERR, "JSON parse error: ", e);
         }
         return devices;
+    }
+
+    public static List<ResourceGroup> ResourceGroupList(JSONObject object) {
+        List<ResourceGroup> resourceGroups = new ArrayList<>();
+        try {
+            JSONArray array = object.getJSONArray("data");
+            JSONObject obj;
+            for (int i = 0; i < array.length(); i++) {
+                obj = array.getJSONObject(i);
+                String name = obj.getString("name");
+                String description = obj.getString("description");
+                String id = obj.getString("_id");
+                resourceGroups.add(new ResourceGroup(name, description, id));
+            }
+        } catch (JSONException e) {
+            Log.e(DEV_ERR, "JSON parse error: ", e);
+        }
+        return resourceGroups;
     }
 }
---
+++ app/src/main/java/com/example/rehome/models/ResourceGroup.java
@@ -0,0 +1,42 @@
+package com.example.rehome.models;
+
+public class ResourceGroup {
+    private String name;
+    private String description;
+    private String id;
+
+    public ResourceGroup() {
+
+    }
+
+    public ResourceGroup(String name, String description, String id) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
